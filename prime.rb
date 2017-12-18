ead5c1fbf150f58785a73d68e1edabe23246ed96
Hoon Lee <yl.hoony@gmail.com>
2017-12-18 00:46:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(number)
+
+  all = (0..number).to_a
+
+  primes[0] = primes[1] = nil
+  
+  counter = 0
+  primes.each do |p|
+    next unless p
+
+    break if p*p > max
+    counter += 1
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+  
+end
