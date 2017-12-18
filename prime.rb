4e073c85f9e556f3c0d00309ebd3b2c6dcc2a09f
Hoon Lee <yl.hoony@gmail.com>
2017-12-18 00:52:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,34 @@
 # Add  code here!
 require 'benchmark'
 
-def prime?(number)
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
 
-  all = (0..number).to_a
-
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
   primes[0] = primes[1] = nil
 
+  # Iterate through primes array
   counter = 0
   primes.each do |p|
+    # Skip if nil
     next unless p
 
+    # Break if we are past the square root of the max value
     break if p*p > max
     counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
     (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
-  primes.compact.include?(number)
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
 end
