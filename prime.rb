c35dbbeae0ec57ca348e56f18c94980996139ae7
Hoon Lee <yl.hoony@gmail.com>
2017-12-18 00:53:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -15,7 +15,7 @@ def sieve(max)
     (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
-  puts "Solved for #{max} in #{counter} steps."
+  # puts "Solved for #{max} in #{counter} steps."
   primes.compact
 end
 
